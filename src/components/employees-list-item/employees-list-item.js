ef3030f68417ea4eddf3513c825f4ec09c0c03bb
Timurblch <timurisspb@gmail.com>
2022-05-08 19:02:52 +0300

    add  test file -> scss with styles

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -16,7 +16,11 @@ const EmployeesListItem = (props) =>{
 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">{name}</span>
+            <span className="list-group-item-label"
+            onClick={onToggleProp}
+            data-toggle="rise"
+                style={{fontSize: '1.5em', transition: 'all', WebkitTransition: 'all', msTransition: 'all'}}>{name}
+            </span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
